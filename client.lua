a2837d08fddc4b884809fc8fb70a261fe1ffa2a0
N3wSk1Y <miner.fedenko@gmail.com>
2022-10-27 21:01:37 +0300

    Refactoring

--- client.lua
+++ client.lua
@@ -1,37 +1,37 @@
-function personalMute(thePlayer, command, name)
-	local player = getPlayerFromName(name)
+function personalMute( thePlayer, command, name )
+	local player = getPlayerFromName( name )
 	if player ~= false
 	then
-		exports.voice:setPlayerMuted(player, true)
-		outputChatBox("Игрок "..getPlayerName(player).." замучен", thePlayer)
+		exports.voice:setPlayerMuted( player, true )
+		outputChatBox( "Игрок "..getPlayerName( player ).." замучен", thePlayer )
 	else
-		outputChatBox("Игрока с таким ником нет на сервере", thePlayer)
+		outputChatBox( "Игрока с таким ником нет на сервере", thePlayer )
 	end
 end
-addCommandHandler("pmute", personalMute)
+addCommandHandler( "pmute", personalMute )
 
-function personalUnMute(thePlayer, command, name)
-	local player = getPlayerFromName(name)
+function personalUnMute( thePlayer, command, name )
+	local player = getPlayerFromName( name )
 	if player ~= false
 	then
-		exports.voice:setPlayerMuted(player, false)
-		outputChatBox("Игрок "..getPlayerName(player).." размучен", thePlayer)
+		exports.voice:setPlayerMuted( player, false )
+		outputChatBox( "Игрок "..getPlayerName( player ).." размучен", thePlayer )
 	else
-		outputChatBox("Игрока с таким ником нет на сервере", thePlayer)
+		outputChatBox( "Игрока с таким ником нет на сервере", thePlayer )
 	end
 end
-addCommandHandler("punmute", personalUnMute)
+addCommandHandler( "punmute", personalUnMute )
 
-function isPlayerMuted(thePlayer, command, name)
-	local player = getPlayerFromName(name)
+function isPlayerMuted( thePlayer, command, name )
+	local player = getPlayerFromName( name )
 	if player ~= false
 	then
-		if exports.voice:setPlayerMuted(player)
+		if exports.voice:setPlayerMuted( player )
 		then
-			outputChatBox("Игрок замучен", thePlayer)
+			outputChatBox( "Игрок замучен", thePlayer )
 		else
-			outputChatBox("Игрок не замучен", thePlayer)
+			outputChatBox( "Игрок не замучен", thePlayer )
 		end
 	end
 end
-addCommandHandler("ismuted", personalUnMute)
+addCommandHandler( "ismuted", personalUnMute )
